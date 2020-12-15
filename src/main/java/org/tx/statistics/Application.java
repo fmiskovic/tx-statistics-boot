d651dfb0c190cd6119a13a004b45f66ffbf9ec87
fmiskovic <fmiskovic@yandex.com>
2020-12-15 21:30:48 +0100

    remove unnecessary configuration

--- src/main/java/org/tx/statistics/Application.java
+++ src/main/java/org/tx/statistics/Application.java
@@ -3,26 +3,12 @@ package org.tx.statistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableAsync
-@EnableCaching
 @EntityScan(basePackages = {"org.tx.statistics"})
 public class Application {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("statistics");
     }
 }
